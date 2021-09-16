fa0d2a7b268ba6693465b5cdf1ac6c33af24d6bf
Dongmin <ehdals338@gmail.com>
2021-09-16 23:50:03 +0900

    Initial commit

---
+++ hotdeal.py
@@ -0,0 +1,20 @@
+import requests
+from bs4 import BeautifulSoup
+
+#New world
+url = "https://www.fmkorea.com/hotdeal"
+#html 소스 가져오기
+res = requests.get(url)
+
+#html parsing
+soup = BeautifulSoup(res.text, 'lxml')
+
+#review list
+ul = soup.find('div', "fm_best_widget _bd_pc")
+lis = ul.find_all('h3')
+
+#review title print
+count = 0
+for li in lis:
+    count += 1
+    print(f"[{count}th] ", li.a.string)
